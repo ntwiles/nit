3f74a53c1e34625c0c6f0a8ec54091f6e74253a7
Nathan Wiles <ntwiles@gmail.com>
2022-02-27 12:57:02 -0500

    Offset line numbers to avoid 0 index.

--- src/difference/mod.rs
+++ src/difference/mod.rs
@@ -16,11 +16,11 @@ pub fn get_diff<'a>(
 
     let removes = hs_a
         .difference(&hs_b)
-        .map(|rm| Delta::removed(rm, hm_a[rm]));
+        .map(|rm| Delta::removed(rm, hm_a[rm] + 1));
 
     let adds = hs_b
         .difference(&hs_a)
-        .map(|add| Delta::added(add, hm_b[add]));
+        .map(|add| Delta::added(add, hm_b[add] + 1));
 
     let mut deltas: Vec<Delta> = removes.chain(adds).collect();
     deltas.sort_by_key(|d| d.line);
@@ -36,7 +36,7 @@ mod tests {
         let old = "a line\nanother line";
         let new = "a line\na diff!\nanother line";
 
-        let expected = vec![Delta::added("a diff!", 1)];
+        let expected = vec![Delta::added("a diff!", 2)];
         let actual = get_diff(old.lines(), new.lines());
 
         assert_eq!(expected, actual);
@@ -47,7 +47,7 @@ mod tests {
         let old = "a line\nanother line";
         let new = "a line\na diff!\nanother diff!\nanother line";
 
-        let expected = vec![Delta::added("a diff!", 1), Delta::added("another diff!", 2)];
+        let expected = vec![Delta::added("a diff!", 2), Delta::added("another diff!", 3)];
         let actual = get_diff(old.lines(), new.lines());
 
         assert_eq!(expected, actual);
@@ -58,7 +58,7 @@ mod tests {
         let old = "a line\nline to remove\na third line";
         let new = "a line\na third line";
 
-        let expected = vec![Delta::removed("line to remove", 1)];
+        let expected = vec![Delta::removed("line to remove", 2)];
         let actual = get_diff(old.lines(), new.lines());
 
         assert_eq!(expected, actual);
@@ -69,7 +69,7 @@ mod tests {
         let old = "a line\nanother line\nremove";
         let new = "a line\nanother line";
 
-        let expected = vec![Delta::removed("remove", 2)];
+        let expected = vec![Delta::removed("remove", 3)];
         let actual = get_diff(old.lines(), new.lines());
 
         assert_eq!(expected, actual);
@@ -80,7 +80,7 @@ mod tests {
         let old = "a line\nremove a\nremove b\nlast line";
         let new = "a line\nlast line";
 
-        let expected = vec![Delta::removed("remove a", 1), Delta::removed("remove b", 2)];
+        let expected = vec![Delta::removed("remove a", 2), Delta::removed("remove b", 3)];
         let actual = get_diff(old.lines(), new.lines());
 
         assert_eq!(expected, actual);
@@ -91,7 +91,7 @@ mod tests {
         let old = "first line\nremove\nlast line";
         let new = "first line\nadd\nlast line";
 
-        let expected = vec![Delta::removed("remove", 1), Delta::added("add", 1)];
+        let expected = vec![Delta::removed("remove", 2), Delta::added("add", 2)];
         let actual = get_diff(old.lines(), new.lines());
 
         assert_eq!(expected, actual);
