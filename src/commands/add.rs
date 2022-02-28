bd1a171ef060cc0fa7b550bc767b8102964b4599
Nathan Wiles <ntwiles@gmail.com>
2022-02-28 03:50:27 -0500

    Add objects crate.

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -1,28 +1,17 @@
-use std::fs::{read_to_string, File};
-use std::io::prelude::*;
+use std::fs::read_to_string;
 
-use crate::hash::sha_256_hex;
-use crate::index::{load_index, write_index};
+use crate::index::{load_index_as_map, write_index};
+use crate::objects::write_object;
 
 pub fn add(mut args: impl Iterator<Item = String>) {
     let file_name = args.next().expect("Must specify which file(s) to add.");
 
     let contents = read_to_string(&file_name).expect("Could not load file contents.");
-    let hash = sha_256_hex(&contents);
 
-    let mut index = load_index();
+    // Check if object already exists.
+    let hash = write_object(&contents);
 
-    if let Some(existing_hash) = index.get(&file_name) {
-        if &hash == existing_hash {
-            return;
-        }
-    }
-
-    let object_file_name = format!(".nv/objects/{}", hash);
-    let mut file = File::create(object_file_name).expect("Could not create object file.");
-
-    file.write_all(contents.as_bytes())
-        .expect("Could not write to object file.");
+    let mut index = load_index_as_map();
 
     index.remove(&file_name).unwrap();
     index.insert(file_name, hash);
---
+++ src/objects.rs
@@ -0,0 +1,16 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+use crate::hash::sha_256_hex;
+
+pub fn write_object(contents: &str) -> String {
+    let hash = sha_256_hex(&contents);
+
+    let object_file_name = format!(".nv/objects/{}", hash);
+    let mut file = File::create(object_file_name).expect("Could not create object file.");
+
+    file.write_all(contents.as_bytes())
+        .expect("Could not write to object file.");
+
+    hash
+}
